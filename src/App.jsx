66ee5225e99a22aaeaa2c25ce0e4c475e384521a
Brocodew <anvukog@gmail.com>
2023-12-01 20:33:53 +0700

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./app.css";
+import { useState } from "react";
+
+/* eslint-disable no-unused-vars */
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [step, setStep] = useState(1);
+  const [valueButton, setValueButton] = useState();
+  const [test, setTest] = useState({ name: "Jonas" });
 
+  const handlePrevious = (e) => {
+    console.log("e", e.target);
+    setValueButton(1);
+    setTest({ name: "Ban" });
+
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  };
+
+  const handleNext = (e) => {
+    console.log("e", e.target);
+    setValueButton(2);
+    setTest({ name: "An" });
+    if (step < 3) {
+      setStep(step + 1);
+    }
+  };
+
+  console.log("test", test);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="steps">
+        <div className="numbers">
+          <div className={step === 1 ? "active" : ""}>1</div>
+          <div className={step === 2 ? "active" : ""}>2</div>
+          <div className={step === 3 ? "active" : ""}>3</div>
+        </div>
+        <p className="message">{messages[step - 1]}</p>
+        <div className="buttons">
+          <button
+            className={valueButton === 1 ? "activeButton" : ""}
+            onClick={(e) => handlePrevious(e)}
+          >
+            Previous
+          </button>
+          <button
+            className={valueButton === 2 ? "activeButton" : ""}
+            onClick={(e) => handleNext(e)}
+          >
+            Next
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
