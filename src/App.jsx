aa4d5fbfc00bbe12e7efae96733aa98484b61ff4
BroCodew <anvukog@gmail.com>
2023-12-02 13:12:42 +0700

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,40 @@
-import "./app.css";
-import { useState } from "react";
+import React from "react";
 
-/* eslint-disable no-unused-vars */
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
-
-function App() {
-  const [step, setStep] = useState(1);
-  const [valueButton, setValueButton] = useState();
-  const [test, setTest] = useState({ name: "Jonas" });
-
-  const handlePrevious = (e) => {
-    console.log("e", e.target);
-    setValueButton(1);
-    setTest({ name: "Ban" });
-
-    if (step > 1) {
-      setStep(step - 1);
-    }
-  };
-
-  const handleNext = (e) => {
-    console.log("e", e.target);
-    setValueButton(2);
-    setTest({ name: "An" });
-    if (step < 3) {
-      setStep(step + 1);
-    }
-  };
-
-  console.log("test", test);
-  return (
-    <>
-      <div className="steps">
-        <div className="numbers">
-          <div className={step === 1 ? "active" : ""}>1</div>
-          <div className={step === 2 ? "active" : ""}>2</div>
-          <div className={step === 3 ? "active" : ""}>3</div>
-        </div>
-        <p className="message">{messages[step - 1]}</p>
-        <div className="buttons">
-          <button
-            className={valueButton === 1 ? "activeButton" : ""}
-            onClick={(e) => handlePrevious(e)}
-          >
-            Previous
-          </button>
-          <button
-            className={valueButton === 2 ? "activeButton" : ""}
-            onClick={(e) => handleNext(e)}
-          >
-            Next
-          </button>
-        </div>
+export default function App(){
+    return(
+      <div className="app">
+        <Logo/>
+        <Form/>
+        <PackingList/>
       </div>
-    </>
-  );
-}
+    );
 
-export default App;
+};
+
+function Logo(){
+  return (
+    <h1>Far Away</h1>
+  )
+}
+function Form(){
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trips</h3>
+    </div>
+  )
+}
+function PackingList(){
+
+  return (
+    <div className="list">
+      List
+    </div>
+  )
+}
+function Stats(){
+  return(
+    <footer>
+      <em>You have X item on your list, and you already packed x(X%)</em>
+    </footer>
+  )
+}
